78b94c00960a9ef3740b70b4dc8559c0472bb1c5
rustielin <rustie117@gmail.com>
2017-01-14 11:47:33 -0800

    ask a tutor button

--- student-ui/scripts/chat/controllers/chat.controller.js
+++ student-ui/scripts/chat/controllers/chat.controller.js
@@ -7,18 +7,34 @@ angular.module("chat").
         console.log("In controller");
         // var firebaseUrl = "https://tutor-chat.firebaseio.com/Sessions/Messages";
 
-        $scope.sessionkey = "Session key here:";
+        $scope.sessionkey = Math.floor(Math.random() * 2147483647);
         $scope.username = "Your name here:";
+
+
+
+        // generate teh queue on startup
+        var firebaseUrl = "https://tutor-chat.firebaseio.com/Queues"+ "/" + $scope.sessionkey;
+        var queueRef = new Firebase(firebaseUrl);
+        var sync = $firebase(queueRef);
+
+        queueRef.set({name: $scope.username})
+
+        // listen for new queues
+        queueRef.on("child_added", function(snapshot) {
+            var queue = snapshot.val();
+
+        })
+
+
+        // fucking changes the session
+        var firebaseUrl = "https://tutor-chat.firebaseio.com/Session"+ "/" + $scope.sessionkey;
+        var chatRef = new Firebase(firebaseUrl);
+        var sync = $firebase(chatRef);
 
         $scope.newMessageKeyPress = function(keyEvent) {
             // whenever enter
             if (keyEvent.which === 13) {
                 console.log("Enter clicked : " + $scope.new_message);
-
-                // fucking changes the session
-                var firebaseUrl = "https://tutor-chat.firebaseio.com/Sessions"+ "/" + $scope.sessionkey;
-                var chatRef = new Firebase(firebaseUrl);
-                var sync = $firebase(chatRef);
 
                 $scope.chat_messages = sync.$asArray();
 
