5555c7a79e405058401fe6e60ab96b5e5d3314c9
rustielin <rustie117@gmail.com>
2017-01-14 09:11:48 -0800

    ugly queue

--- chat_client.py
+++ chat_client.py
@@ -59,12 +59,6 @@ if __name__ == '__main__':
     while (True):
         message = raw_input("")
 
-        # with num as open("numbers.txt", "r+"):
-        #     counter = int(num.nextline())
-        #     counter += 1
-        #     num.write(counter)
-        #
-
         print("\n")
         fb.post('/Sessions/Messages',
                 {
