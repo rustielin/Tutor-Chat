0f18e4bd004e491b4b49c4285012bf220978c9fe
rustielin <rustie117@gmail.com>
2017-01-14 04:51:14 -0800

    chat w/ firebase

---
+++ ui-client/scripts/chat/services/chat.service.js
@@ -0,0 +1,16 @@
+/**
+ * Created by mayureshp on 2/17/2015.
+ */
+
+angular.module("chat").
+    factory("ChatService", ["$firebase", function($firebase) {
+        console.log("In service");
+        var firebaseUrl = "https://tutor-chat.firebaseio.com/PythonChatDemo/Track";
+        var chatRef = new Firebase(firebaseUrl);
+
+        chatRef.on("child_added", function(snapshot) {
+            var chat_message = snapshot.val();
+            console.log("Chat message = " + chat_message);
+        })
+
+    }]);
