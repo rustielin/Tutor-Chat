0e51839b084d6146a654ce1cceae799775cd8e5b
rustielin <rustie117@gmail.com>
2017-01-14 21:19:50 -0800

    working but chats insert in middle ugh

--- ta-ui/scripts/chat/controllers/chat.controller.js
+++ ta-ui/scripts/chat/controllers/chat.controller.js
@@ -11,7 +11,6 @@ angular.module("chat").
         var firebaseUrl = "https://tutor-chat.firebaseio.com/Queues";
         var chatRef = new Firebase(firebaseUrl);
 
-
         var sync = $firebase(chatRef);
         //initialize messages just in case
         $scope.chat_messages = sync.$asArray();
@@ -30,22 +29,42 @@ angular.module("chat").
             });
         });
 
+        // connect with some guy via queue, start a chat session, then kill his queue
+        $scope.connectQueue = function() {
+          chatRef.on('value', function(snapshot) {
+            // var snap = snapshot.child();
+            // console.log("VALLL  " + snap.name);
+              snapshot.forEach(function(childSnapshot) {
+                var childData = childSnapshot.val();
+                var queue = childData.name;
+                if (queue === parseInt(queue, 10)) {
+                  // alert(queue);
+                  $scope.queue = queue;
+                  return;
+                }
+              });
+          });
+
+          $scope.kill_queue();
+        }
+
         $scope.kill_queue = function() {
           chatRef.child($scope.queue).remove();
         }
+
         $scope.newMessageKeyPress = function(keyEvent) {
             // whenever enter
             if (keyEvent.which === 13) {
 
                 // $scope.kill_queue();
-
                 var firebaseUrl_message = "https://tutor-chat.firebaseio.com/Sessions" + "/" + $scope.queue;
                 var chatRef_message = new Firebase(firebaseUrl_message);
-                var sync = $firebase(chatRef_message);
+                var sync_message = $firebase(chatRef_message);
 
                 console.log("Enter clicked : " + $scope.new_message);
-                $scope.chat_messages = sync.$asArray();
+                $scope.chat_messages = sync_message.$asArray();
 
+                // no empty string messages
                 if ($scope.new_message != "") {
                     $scope.chat_messages.$add({name: $scope.username, message: $scope.new_message, $priority: Date.now()});
                     $scope.new_message = "";
