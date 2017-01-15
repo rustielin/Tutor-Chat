82d87e2065602c2f67373b9331a55ed67a512a3e
Nadir Akhtar <nadirakhtar123@gmail.com>
2017-01-14 16:43:48 -0800

    Update ta-ui to match student-ui

--- ta-ui/scripts/chat/controllers/chat.controller.js
+++ ta-ui/scripts/chat/controllers/chat.controller.js
@@ -8,22 +8,23 @@ angular.module("chat").
         // var firebaseUrl = "https://tutor-chat.firebaseio.com/Sessions/Messages";
 
         $scope.sessionkey = "Session key here:";
-        $scope.username = "Your name here";
+        $scope.username = "";
+
+        // fucking changes the session
+        var firebaseUrl = "https://tutor-chat.firebaseio.com/Sessions"+ "/" + $scope.sessionkey;
+        var chatRef = new Firebase(firebaseUrl);
+        var sync = $firebase(chatRef);
 
         $scope.newMessageKeyPress = function(keyEvent) {
             // whenever enter
             if (keyEvent.which === 13) {
                 console.log("Enter clicked : " + $scope.new_message);
-
-                // fucking changes the session
-                var firebaseUrl = "https://tutor-chat.firebaseio.com/Sessions"+ "/" + $scope.sessionkey;
-                var chatRef = new Firebase(firebaseUrl);
-                var sync = $firebase(chatRef);
-
                 $scope.chat_messages = sync.$asArray();
 
-                $scope.chat_messages.$add({name: $scope.username, message: $scope.new_message, $priority: Date.now()});
-                $scope.new_message = "";
+                if ($scope.new_message != "") {
+                    $scope.chat_messages.$add({name: $scope.username, message: $scope.new_message, $priority: Date.now()});
+                    $scope.new_message = "";
+                }
             }
         }
 
